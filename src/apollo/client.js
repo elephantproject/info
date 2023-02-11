5390b8b848f8706f8dce5ce51f032531044f9f8a
elephantproject <elephantproejct@protonmail.com>
2023-02-11 09:31:30 -0800

    asdf

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -28,7 +28,7 @@ export const stakingClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://harmony-graph.elk.finance/subgraphs/name/elkfinance/harmony-blocks',
+    uri: 'https://api.thegraph.com/subgraphs/name/elephantproject/harmony-blocks',
   }),
   cache: new InMemoryCache(),
 })
